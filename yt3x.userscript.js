41346a88d92f62c133ab2f0795efd9cebf6e174a
turbo <me@turbo.ooo>
2021-12-07 16:54:33 -0500

    feat: add duration / 3

--- yt3x.userscript.js
+++ yt3x.userscript.js
@@ -30,12 +30,68 @@ const waitElementsLoaded = async (...elementsQueries) => {
     )
 }
 
+const getFormattedTime = (seconds, precise) => {
+    seconds = Math.max(seconds, 0);
+        
+    const hours = Math.floor(seconds / 60 / 60);
+    const minutes = Math.floor(seconds / 60) % 60;
+    let minutesDisplay = String(minutes);
+    let secondsNum = seconds % 60;
+    if (!precise) {
+        secondsNum = Math.floor(secondsNum);
+    }
+
+    let secondsDisplay = String(precise ? secondsNum.toFixed(3) : secondsNum);
+    
+    if (secondsNum < 10) {
+        //add a zero
+        secondsDisplay = "0" + secondsDisplay;
+    }
+    if (hours && minutes < 10) {
+        //add a zero
+        minutesDisplay = "0" + minutesDisplay;
+    }
+    if (isNaN(hours) || isNaN(minutes)) {
+        return null;
+    }
+
+    const formatted = (hours ? hours + ":" : "") + minutesDisplay + ":" + secondsDisplay;
+
+    return formatted;
+}
+
+const speedUp = (videoElement) => {
+    videoElement.playbackRate = 3
+
+    // YouTube player time display
+    const display = document.querySelector(".ytp-time-display.notranslate")
+    if (!display) return
+
+    const durationID = "durationAfterSpeedUp"
+    let duration = document.getElementById(durationID)
+
+    // Create span if needed
+    if (duration === null) {
+        duration = document.createElement('span')
+        duration.id = durationID
+        duration.classList.add("ytp-time-duration")
+
+        display.appendChild(duration)
+    }
+
+    const durationAfterSpeedUp = getFormattedTime(videoElement.duration / 3)
+
+    duration.innerText = (durationAfterSpeedUp == null || skippedDuration <= 0) ? "" : " (" + durationAfterSpeedUp + ")";
+
+}
+
 ;(async () => {
+
     await waitElementsLoaded('video').then(() => {
-        document.querySelector('video').playbackRate = 3
+        speedUp(document.querySelector('video'))
     })
 
     window.addEventListener('yt-page-data-updated', () => {
-        document.querySelector('video').playbackRate = 3
+        speedUp(document.querySelector('video'))
     })
 })()
